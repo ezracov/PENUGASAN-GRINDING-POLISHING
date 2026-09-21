199b16418d842a01b21c748c68598ab1982e9f4f
ezracov <ezracov@gmail.com>
2026-09-21 12:12:51 +0700

    Delete js directory

--- js/supabase-client.js
+++
@@ -1,7 +0,0 @@
-// Sertakan library Supabase via CDN di HTML:
-// <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-
-const SUPABASE_URL = "https://lagjhociwnovvkolaexh.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_iHdkQs1eGTwPhYqdrjb8ow_I5XPGAV3";
-
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
